1a02294f47a18dc1b6f07a4830014731a0bda4a0
fabiojr933 <fabiojr933@gmail.com>
2021-08-16 22:20:10 -0400

    iniciando o projeto de controle financeiro

---
+++ app/views/home.php
@@ -0,0 +1,187 @@
+<div class="page-head">
+    <h4 class="mt-2 mb-2">Sweet Alert</h4>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A basic message</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-basic">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>Success example</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-success">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A title with a text under</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-title">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>An error message</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-error">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title  mb-4">
+                    <h6>A modal window with a long content inside:</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="long-text">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>Custom HTML description and buttons with ARIA labels</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="custom-html-alert">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A custom positioned dialog</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="position">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A warning message, with a function attached to the "Confirm"-button...</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-warning">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>By passing a parameter, you can execute something else for "Cancel".</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-params">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A message with a custom image and CSS animation disabled</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="custom-image">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>A message with auto close timer</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="sa-close">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>Ajax request example</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="ajax-alert">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>Chaining modals (queue) example</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="chaining-alert">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-12">
+        <div class="card bg-white m-b-30">
+            <div class="card-body text-center">
+                <div class="card-title mb-4">
+                    <h6>Dynamic queue example</h6>
+                </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-primary" id="dynamic-alert">Click me!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end row-->
---
+++ app/views/template.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mannat Themes">
+    <meta name="keyword" content="">
+
+    <title>SYNTRA | Responsive Web App Kit</title>
+
+
+
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- Sweet Alert css -->
+    <link href="assets/plugins/sweet-alert/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="<?php echo URL_BASE ?>assets/images/favicon.ico">
+    <link href="<?php echo URL_BASE ?>assets/plugins/morris-chart/morris.css" rel="stylesheet">
+    <link href="<?php echo URL_BASE ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo URL_BASE ?>assets/css/slidebars.min.css" rel="stylesheet">
+    <link href="<?php echo URL_BASE ?>assets/css/icons.css" rel="stylesheet">
+    <link href="<?php echo URL_BASE ?>assets/css/menu.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo URL_BASE ?>assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body class="sticky-header">
+    <section>
+        <?php include "menu.php" ?>
+        <div class="body-content">
+            <?php include "cabecalho.php" ?>
+            <div class="container-fluid">
+                <?php $this->load($view, $viewData); ?>
+            </div>
+            <?php include "rodape.php" ?>
+        </div>
+
+    </section>
+    <!-- jQuery -->
+    <script src="<?php echo URL_BASE ?>assets/js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/jquery-migrate.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/modernizr.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/js/slidebars.min.js"></script>
+    <!--plugins js-->
+    <script src="<?php echo URL_BASE ?>assets/plugins/counter/jquery.counterup.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/plugins/sparkline-chart/jquery.sparkline.min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/pages/jquery.sparkline.init.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/plugins/chart-js/Chart.bundle.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/plugins/morris-chart/raphael-min.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/plugins/morris-chart/morris.js"></script>
+    <script src="<?php echo URL_BASE ?>assets/pages/dashboard-init.js"></script>
+    <!--app js-->
+    <script src="<?php echo URL_BASE ?>assets/js/jquery.app.js"></script>
+
+    <!-- Sweet Alert Js  -->
+    <script src="assets/plugins/sweet-alert/sweetalert2.min.js"></script>
+    <script src="assets/pages/jquery.sweet-alert.init.js"></script>
+    <script>
+        jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 100,
+                time: 1200
+            });
+        });
+    </script>
+</body>
+
+</html>
